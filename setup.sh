b8d4738b2b38f22905f840bdbbb0a4c17cde0acd
awsmith <aws@awsmith.us>
2022-04-25 14:28:57 -0500

    Add Cinnamon desktop configuration
    Yay for storing large binary files in git!  Unfortunately, for many of
    these files, there is no workable alternative: some are inherently
    binary, while others can only be built via an ad hoc series of scripts
    after installing a massive dependency chain that I wouldn't use for
    anything else.  Thus, binaries.

--- setup.sh
+++ setup.sh
@@ -2,10 +2,26 @@
 # Copyright (c) 2022, Andrew Smith <aws@awsmith.us> :::::::::::: SOLI DEO GLORIA
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
+# SUMMARY
+# =======
 # This script performs one-time setup tasks that are only necessary for fresh
 # operating system installations.  In order to prevent these tasks from being
 # executed multiple times, it creates an empty file "~/.awsmith-setup": if this
 # file exists, this script will refuse to run.
+#
+# OPTIONAL PROCEDURES
+# ===================
+# - Cinnamon
+#   - To load a full dump of the Cinnamon settings, run:
+#       $ dconf load /org/cinnamon/ < optional-setup-do-not-stow/cinnamon/cinnamon.ini
+#       $ dconf load /org/gnome/ < optional-setup-do-not-stow/cinnamon/gnome.ini
+#       $ dconf load /org/gtk/ < optional-setup-do-not-stow/cinnamon/gtk.ini
+#       $ dconf load /org/nemo/ < optional-setup-do-not-stow/cinnamon/nemo.ini
+#     Cinnamon will likely need to be restarted once these commands are run.
+#   - The directory referenced above also contains a number of JSON files.  Each
+#     of these files contains the configuration for a single applet: these
+#     configurations will need to be restored manually through each applet's
+#     settings.
 
 if [ -e $HOME/.awsmith-setup ]; then
     echo "This system is already set up!"
@@ -34,4 +50,5 @@ for id in "${vscode_extension_ids[@]}"; do
     fi
 done
 
+echo "Done! See the source of this script for more setup actions that cannot be automated."
 touch $HOME/.awsmith-setup
